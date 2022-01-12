e5ed8c2b09a20990a2b06580bb8157f799c8b109
gohelvivek <marubhautik97@gmail.com>
2022-01-12 13:38:19 +0530

    new

---
+++ kotlinprg/messagebox/app/src/main/java/com/avh/messagebox/activity_after_notification.kt
@@ -0,0 +1,15 @@
+package com.avh.messagebox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_after_notification : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_after_notification)
+
+
+
+    }
+
+}
